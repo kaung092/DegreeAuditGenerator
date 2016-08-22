348a69499cf72a63f473b02a8498d8dc1e25ed9e
root <root@ip-172-31-54-41.ec2.internal>
2016-08-22 19:38:00 +0000

    first commit

---
+++ generateExcel.php
@@ -0,0 +1,219 @@
+<?php
+		if(isset($_GET['text']))
+		{
+			$text = $_GET['text'];
+
+			$input_lines = explode("\n", $text);
+			$EEcur = file_get_contents("EEcur.json"); //EE curriculum 
+			$EEcur_json = json_decode($EEcur, true);
+		
+
+			$beg_s='<Cell ss:StyleID="s62"><Data ss:Type="String">';
+			$beg_sGray='<Cell ss:StyleID="s7"><Data ss:Type="String">';
+			$end='</Data></Cell>';
+
+			$output_M=array();	
+			$output_S=array();	
+			$output_EGR=array();	
+			$output_EEC=array();	
+			$output_EER=array();	
+			$output_EEL=array();	
+			$output_LE=array();	
+			$output_LA=array();	
+			$output_ENL=array();	
+			$output_Oth=array();	
+
+
+			foreach($input_lines as $line)
+			{
+				if($line == null){}
+				else{
+				$line1 = preg_replace("/\s+/"," ",$line);
+				$line = explode(" ",$line1);
+
+				$code = $line[1].' '.$line[2];
+
+				$row['classCode'] = $line[1].' '.$line[2];
+				$row['semester'] = $line[0];
+				$row['credit'] = $line[3];
+
+				if(strlen($line[4]) <=2) //The grade column is not blank
+				{
+					$row['grade'] = $line[4];
+					$index = 5;
+					$beg_s ='<Cell ss:StyleID="s62"><Data ss:Type="String">'; 
+				}
+				else
+				{
+					$row['grade'] = 'IP';
+					$beg_s = $beg_sGray; //highlight "In Progress" course
+					$index =4;
+				}
+
+				$row['category'] =  'Others';
+				$Cat = 'Others';	
+				$row['class']= "";
+				for($s= $index;$s<count($line);$s++)
+				{
+					$row['class'] .= $line[$s]." ";
+				}
+
+
+				$Cat2 ="";
+				foreach($EEcur_json as $key=>$EEcur)
+				{
+					if($code  == $key)
+					{
+						$Cat = $EEcur['category'];
+						$Cat2 = $EEcur['category2'];
+
+						$F_grade =isset($EEcur['F_grade'])?$EEcur['F_grade']:null;
+						if($or == null) //Courses that are interchangable with other classes
+						{
+							$or = isset($EEcur['or'])?$EEcur['or']:null;
+						}
+						$input_array[$code]['category'] = $Cat;	
+						$row['category'] = $Cat;
+					}
+				}
+
+
+			$output='';
+			$output.=$beg_s.$row['semester'].$end."\n";
+			$output.=$beg_s.$row['classCode'].$end."\n";
+			$output.=$beg_s.$row['credit'].$end."\n";
+			$output.=$beg_s.$row['grade'].$end."\n";
+			$output.=$beg_s.$row['class'].$end."\n";
+
+				if($row['grade']=="W" || $row['grade']=="F" || $row['grade']=="#F")
+				{}
+				else
+				{
+						if($Cat == 'MATHEMATICS' )
+						{
+							if($row['grade']=="CR" || $row['grade']=="IP")
+							{
+								$output_M.array_push($output_M,$output);
+								asort($output_M);
+							}
+							else if(isset($F_grade) && strnatcmp ($row['grade'],$F_grade ) < 0) // if grade greater than failing grade
+							{
+								$output_M.array_push($output_M,$output);
+								asort($output_M);
+							}
+							else
+							{
+								//Did not meet minimum grade. skipping
+							}
+						}
+						else if($Cat == 'SCIENCES')
+						{
+							if($row['grade']=="CR" || $row['grade']=="IP")
+							{
+								$output_S.array_push($output_S,$output);
+								asort($output_S);
+							}
+							else if(isset($F_grade) && strnatcmp ($row['grade'],$F_grade ) < 0) // if grade greater than failing grade
+							{
+								$output_S.array_push($output_S,$output);
+								asort($output_S);
+							}
+							else
+							{
+								//Did not meet minimum grade. skipping
+							}
+						}
+						else if($Cat == 'ENGINEERING')
+						{
+							$output_EGR.array_push($output_EGR,$output);
+							asort($output_EGR);
+						}
+						else if($Cat == 'EE CORE')
+						{
+							$output_EEC.array_push($output_EEC,$output);
+							asort($output_EEC);
+						}
+						else if($Cat == 'EE RESTRICTED ELECTIVES')
+						{
+							if(count($output_EER)<2)//Show only 2
+							{
+								$output_EER.array_push($output_EER,$output);
+								asort($output_EER);
+							}
+							else{
+								$Cat = $Cat2;
+							}
+						}
+						if($Cat == 'EE LAB ELECTIVES')
+						{
+							if(count($output_EEL)<2)
+							{
+								$output_EEL.array_push($output_EEL,$output);
+								asort($output_EEL);
+							}
+						}
+						if($Cat == 'LECTURE ELECTIVES')
+						{
+							if(count($output_LE)<10) //show only 10 courses
+							{
+								$output_LE.array_push($output_LE,$output);
+								asort($output_LE);
+							}
+						}
+						if($Cat == 'LIBERAL ARTS')
+						{
+							$output_LA.array_push($output_LA,$output);
+							asort($output_LA);
+						}
+						if($Cat == 'ENGLISH')
+						{
+							if($code == $or) //interchangable course already added
+							{
+								$or = null;
+							}
+							else
+							{
+								$output_ENL.array_push($output_ENL,$output);
+								asort($output_ENL);
+							}
+						}
+						if($Cat == 'Others')
+						{
+							if($row['grade']!="F" || $row['grade']!="W")
+							{
+								$output_Oth.array_push($output_Oth,$output);
+								asort($output_Oth);
+							}
+						}
+
+				}
+
+
+
+				$i++;
+				}
+			}
+
+
+
+			//Add Extra Row for EE Restricted category
+			$output='';
+			$output.=$beg_s.' '.$end."\n";
+			$output.=$beg_s.' '.$end."\n";
+			$output.=$beg_s.' '.$end."\n";
+			$output.=$beg_s.' '.$end."\n";
+			$output.=$beg_s.' '.$end."\n";
+			$output_EER.array_push($output_EER,$output);
+
+			include('HA_xcel.php');
+			
+
+			echo (json_encode($input_array));//Not really doing anything with it
+			
+
+
+			
+		}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,66 @@
+<?php $version = '2014101477';  ?>
+
+<head>
+  <title>Student Degree Audit Generator</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css<?php echo "?ver=$version"; ?>">
+  <link rel="stylesheet" href="index.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js<?php echo "?ver=$version"; ?>"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js<?php echo "?ver=$version"; ?>"></script>
+	<script src="index.js<?php echo "?ver=$version"; ?>" language="JavaScript" type="text/javascript"></script>
+</head>
+<body>
+
+<?php
+	$placeholder = "Example:
+2013-02   ENGR   276     3.00  A      Engrng Economics";
+
+
+?>
+
+
+<div class="container">
+  <h2>Generate Student Degree Audit</h2>
+
+  <p>
+		Copy and paste student's class information in text area below:
+	</p>
+  <form role="form">
+    <div class="form-group">
+      <textarea placeholder="<?php echo $placeholder ?>"style="height:50%;width:50%;display:inline-block;" class="form-control" rows="5" id="student_info"></textarea>
+			<span id="egspan">
+					<p style="font-size:1.5em;">Example Student Data</p>
+					<p id="egdata">
+					2012-02      CSC    		102    	 3.00  	B+     Intro Computing<br>
+					2013-02      ECO    		102.50 	 3.00  	A      Prin Microeconomics<br>
+					2013-09      EE     		241    	 3.00  	B+     Electronics 1<br>
+					2015-09      EE     		428    	 1.00  	B      Photonics Engr Lab<br>
+					2015-09      ENGR   		230    	 3.00  	A-     Thermodynamics<br>
+					transfer  	 MATH   		201    	 3.00  	CR     Calculus 1<br>
+					transfer  	 MATH   	  202    	 3.00  	CR     Calculus 2<br>
+					2012-02   	 PHYS   	  207    	 4.00  	B-     General<br>
+					2014-09   	 EE     		344    	 3.00  	B-     Dgtl Comp Systems<br>
+					2015-02   	 EE     		371    	 3.00 	D      Linear Feedbak Syst<br>
+					2012-02   	 ENGL   		210.07 	 3.00  	B-     Writing-Engineers<br>
+					2013-02   	 ENGR		    204    	 3.00  	B      Electrical Circuits
+					</p>
+			</span>
+			<br>
+			<!--
+			<label>Student Last Name</label>
+      <textarea placeholder="<?php echo 'Last Name' ?>"style="height:40px;width:300px;" class="form-control" rows="5" id="name"></textarea>
+			-->
+			<br>
+			Note: Currently, only EE curriculum is integrated. Most liberal Arts and other courses will be listed under "Others" category
+			<br>
+			<br>
+			<button id="generate" style="background:lightgreen;border:1px solid;" type="button" class="btn btn-success-outline">Generate Excel</button>
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
+
+
